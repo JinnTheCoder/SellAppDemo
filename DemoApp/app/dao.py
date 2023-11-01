cc5114da7f862c0c71aef1be0f5434475360c137
Quốc <2051012098quoc@ou.edu.vn>
2023-11-01 14:56:53 +0700

    Find and Add Product

--- DemoApp/app/dao.py
+++ DemoApp/app/dao.py
@@ -1,57 +1,12 @@
-def load_categories():
-    return [{
-        'id': 1,
-        'name': 'Laptop',
-        'link': 'https://shopee.vn/product/645954084/20584773162?gclid=EAIaIQobChMIjpX_h86QggMVoNQWBR1tSQu9EAQYASABEgIyMvD_BwE'
-    }, {
-        'id': 2,
-        'name': 'PC',
-        'link': ''
-    }]
+from DemoApp.app.models import Category, Product
 
-def load_product(kw):
-    products = [{
-            "id": 1,
-            "name": "iPhone 7 Plus",
-            "description": "Apple, 32GB, RAM: 3GB, iOS13",
-            "price": 17000000,
-            "image":
-                "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-            "category_id": 1
-        }, {
-            "id": 2,
-            "name": "iPad Pro 2020",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image":
-                "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-            "category_id": 2
-        }, {
-            "id": 3,
-            "name": "Galaxy Note 10 Plus",
-            "description": "Samsung, 64GB, RAML: 6GB",
-            "price": 24000000,
-            "image":
-                "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
-            "category_id": 1
-        }, {
-            "id": 1,
-            "name": "iPhone 7 Plus",
-            "description": "Apple, 32GB, RAM: 3GB, iOS13",
-            "price": 17000000,
-            "image":
-                "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
-            "category_id": 1
-        }, {
-            "id": 2,
-            "name": "iPad Pro 2020",
-            "description": "Apple, 128GB, RAM: 6GB",
-            "price": 37000000,
-            "image":
-                "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
-            "category_id": 2
-    }]
+def load_categories():
+    return Category.query.all()
+
+def load_product(kw=None):
+    products = Product.query
 
     if kw:
-        products = [p for p in products if p['name'].find(kw) >= 0]
-    return products
+        products = products.filter(Product.name.contains(kw))
+
+    return products.all()
--- DemoApp/app/models.py
+++ DemoApp/app/models.py
@@ -1,18 +1,35 @@
 from DemoApp.app import db, app
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Category(db.Model):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), nullable=False, unique=True)
+    products = relationship('Product', backref='category', lazy=True)
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    price = Column(Float, default=0)
+    image = Column(String(255))
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
 if __name__ == '__main__':
     with app.app_context():
-        c1 = Category(name='Mobile')
-        c2 = Category(name='Tablet')
-
-        db.session.add(c1)
-        db.session.add(c2)
+        # c1 = Category(name='Mobile')
+        # c2 = Category(name='Tablet')
+        c3 = Category(name='Laptop')
+        c4 = Category(name='PC')
+        # db.session.add_all([c1, c2])
+        db.session.add_all([c3, c4])
+        # db.session.commit()
+        # p1 = Product(name='iPhone 7 Plus', price='3000000', image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg', category_id='1')
+        # p2 = Product(name='iPad Pro 2020', price='10000000', image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg', category_id='2')
+        # p3 = Product(name='Galaxy Note 10 Plus', price='2500000', image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg', category_id='1')
+        # p4 = Product(name='iPhone 10 Plus', price='4500000', image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg', category_id='1')
+        # p5 = Product(name='iPad Pro 2021', price='15000000', image='https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg', category_id='2')
+        # db.session.add_all([p1, p2, p3, p4, p5])
         db.session.commit()
-        # db.create_all()
+        db.create_all()
